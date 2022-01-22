766172845ce05b6fc07e21749f492751278c4c59
moayadabukhadra <moayadabukhadra54@gmail.com>
2022-01-22 16:35:38 +0200

    todo-js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,20 @@ var person = prompt ("please enter your name");
 var gender = prompt ("enter ur gender (male/female)");
 
 var age = prompt ("enter your age");
+while (age <= 0) {
+    
+ 
+
+if (age <= 0){
+
+    var age = prompt ("the age cant be 0 or less please enter ur age again ");
+ } 
+
+ if (age <= 0){
+ 
+     age = prompt ("the age cant be 0 or less please enter ur age again ");
+ }
+}
 
 confirm ("do you want to skip the welcoming message")
 
@@ -18,6 +32,9 @@ alert ("Hello Ms " + person)
 }else {
     alert ("Hello " + person);
 }
+
+
+
 
 
 
