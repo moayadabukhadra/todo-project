580dbaf0e0af08dbd6e4c3156a38eeb4e749184f
moayadabukhadra <moayadabukhadra54@gmail.com>
2022-01-23 20:40:38 +0200

    todo-js2

--- js/app.js
+++ js/app.js
@@ -1,43 +1,79 @@
 'use strict';
 
 var person = prompt ("please enter your name");
-
-console.log(person);
+if (person==""){
+  person= "invaild"
+}
 var gender = prompt ("enter ur gender (male/female)");
-
+if (gender== ""){
+  gender="invaild";
+}
 var age = prompt ("enter your age");
-console.log(age);
+if(age== ""){
+age ="invaild";
+}
 while (age <= 0) {
     
  
+      var age = prompt ("the age cant be 0 or less please enter your age again ");
+   } 
 
-if (age <= 0){
-
-    var age = prompt ("the age cant be 0 or less please enter ur age again ");
- } 
-
- if (age <= 0){
+function visit() {
+  visit =  prompt ("are you new to this website ?");
  
-     age = prompt ("the age cant be 0 or less please enter ur age again ");
- }
+  
 }
 
- var skip = confirm ("do you want to skip the welcoming message")
-console.log(gender);
-console.log(skip);
+visit()
+if (visit == ""){
+  visit = "invaild";
+}
+
+
+function color() {
+  color =   prompt ("what is your favorite color");
+
+
+}
+color()
+if (color == ""){
+  color = "invaild";
+}
+
+
+function from() {
+ from =  prompt ("where are you from");
+
+}
+from()
+if (from == ""){
+  from = "invaild";
+}
+
+var skip = confirm ("do you want to skip the welcoming message")
+let answers = [age,gender,person,skip,visit,color,from]
  
-if (gender == "male" && skip == false ){
+
+
+
+
+
+
+ if (gender == "male" && skip == false ){
+
 
 alert ("hello Mr " + person);
 
-}else if (gender == "female" && skip == false ){
+} if (gender == "female" && skip == false ){
 alert ("Hello Ms " + person);
 }
-if  (gender == "" && skip == false ){
+else if  (gender == "invaild" && skip == false ){
 alert ("Hello " + person );
- 
+ var i =0;
 }
-
+for (i=0 ; i<=6 ; i++){
+  console.log(answers[i]);
+}
 
 
 
